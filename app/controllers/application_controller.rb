d08ceaf3d31b3a5c71c0c56974bb34b166382e51
Curtis Chadwell <laurencechadwell@gmail.com>
2020-06-04 21:05:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+  
+  #reads all articles/displays all articles
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end  
+  
+  #new article
+  get '/articles/new' do
+   @article = Article.new
+   erb :new
+    
+  end
+  
+  #creates the article
+  post '/articles' do 
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+  #shows the article
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
   end
 end
