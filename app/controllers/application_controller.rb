0206f06a5fa7e996c6d2d1857a8c1c3a504c24b0
chrisb3546 <chrisb3546@yahoo.com>
2020-06-06 14:55:35 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,32 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
 
-  get '/' do
+
+  
+  get '/articles' do 
+    @article = Article.all
+
+    erb :index
   end
+  
+  get '/articles/new' do
+
+    erb :'articles/new'
+   end
+  
+   get '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    erb  :'articles/show'
+
+  end
+
+  post '/articles/:id' do 
+    article = Article.create(params)
+
+    redirect "articles/#{article.id}"
+  end
+   
 end
