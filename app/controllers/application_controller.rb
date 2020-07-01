58df87afff4b187b3dee0133aebc2c772da196f6
swade781977 <swade781977@yahoo.com>
2020-06-30 20:00:52 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,30 @@ class ApplicationController < Sinatra::Base
 
   get '/articles' do
     @articles = Article.all
-    erb :'index'
+    erb :'/index'
+  end
+
+  get '/articles/new' do
+  
+    erb :'/new'
+  end
+
+  post 'articles' do
+    Article.create(params[])
+    erb :show/params[:id]
   end
 
   get '/articles/:id' do
-    
+    @article = Article.find_by_id(params[:id])
+
+    if @article 
+      erb :'/show'
+    else 
+      "No article found with that id."
+    end
+  end
+
+  
+
+
 end
