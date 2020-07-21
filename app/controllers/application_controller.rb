a70c5937b52cbc5f1c184487346a3bd80e6e105f
arnie b <arnold.bwaila@gmail.com>
2020-07-21 09:10:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +9,32 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do 
+    erb :new
+  end
+  
+  post '/articles' do 
+    title = params[:title]
+    content = params[:content]
+    new_art = Article.create(title: title,content: content)
+    @article = new_art
+    erb :show
+  end
+  
+  get '/articles/:id' do 
+    @article = params[:id]
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @artice = Article.find(id: params[:id])
+    erb :edit
+  end
+  
+  
 end
